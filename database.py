6b7467b811642adda97786c6940bee1cd4cc23ae
Joseph Orokpo <101102478+Josephorokpo@users.noreply.github.com>
2024-04-03 10:21:38 +0100

    Update database.py

--- database.py
+++ database.py
@@ -1,42 +1,28 @@
-from sqlalchemy import create_engine, text 
+from sqlalchemy import create_engine, text
 import os
 
-db_connection_string =  os.environ['db_connection_string']
+db_connection_string = os.environ['DB_CONNECTION_STRING']
 
-engine = create_engine(
-  db_connection_string,
-  connect_args={
-    "ssl": {
-      "ssl_ca": "/etc/ssl/cert.pem"
-    }
-  }
-)
+engine = create_engine(db_connection_string,
+                       connect_args={"ssl": {
+                           "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
+
 
 def load_jobs_from_db():
   with engine.connect() as conn:
     result = conn.execute(text("select * from jobs"))
     jobs = []
-    for row in result.fetchall():
-          row_dict = {}
-          for column, value in zip(result.keys(), row):
-              row_dict[column] = value
-          jobs.append(row_dict)
+    for row in result.all():
+      jobs.append(dict(row))
     return jobs
-    
+
+
 def load_job_from_db(id):
-    with engine.connect() as conn:
-        result = conn.execute(
-            text("select * from jobs where id = :val"), {"val": id}
-        )
-        rows = result.fetchall()
-        print("Rows fetched for ID:", id, ":", rows)
-        if len(rows) == 0:
-            return None
-        else:
-            job_dicts = []
-            for row in rows:
-                job_dict = {}
-                for column, value in zip(result.keys(), row):
-                    job_dict[column] = value
-                job_dicts.append(job_dict)
-            return job_dicts
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"), val=id)
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else:
+      return dict(rows[0])
