cdd44af9fb7bd0b5935010173c36d967097966c0
Joseph Orokpo <101102478+Josephorokpo@users.noreply.github.com>
2024-04-03 09:13:36 +0000

    update database.py

--- database.py
+++ database.py
@@ -17,17 +17,16 @@ def load_jobs_from_db():
       jobs.append(dict(zip(result.keys(), row)))
     return jobs
 
+
 def load_job_from_db(id):
   with engine.connect() as conn:
-    result = conn.execute(
-      text("SELECT * FROM jobs WHERE id = :val"),
-      {'val': id}
-    )
+    result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"),
+                          {'val': id})
     rows = result.all()
     if len(rows) == 0:
       return None
     else:
-      return dict(zip(result.keys(), rows[0])) 
+      return dict(zip(result.keys(), rows[0]))
 
 
 def add_application_to_db(job_id, data):
